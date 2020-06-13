e6ba403504dcc7adb99957154a605ab3a4849ae7
kimmypracha <pracha.promtaow@gmail.com>
2020-06-14 01:17:19 +0700

    test direct-production mode

--- config/config.js
+++ config/config.js
@@ -1,1 +1,2 @@
-module.exports = require('./env/'+ process.env.NODE_ENV + ".js");
+//module.exports = require('./env/'+ process.env.NODE_ENV + ".js");
+module.exports = require('./env/production.js');
