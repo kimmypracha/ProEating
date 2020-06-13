642990ba614fc9c23a10dada9f5f14d52b360f63
kimmypracha <pracha.promtaow@gmail.com>
2020-06-14 01:06:27 +0700

    add temporary mongouri for production mode

--- config/env/production.js
+++ config/env/production.js
@@ -1,4 +1,4 @@
 module.exports = {
     debug : false,
-    mongoUri : 'mongodb://localhost/proeating_db'
+    mongoUri : 'mongodb://heroku_gzvnl6hh:jba968usbh58i073d32ca8odqc@ds221435.mlab.com:21435/heroku_gzvnl6hh'
 }
