9945a16ccb26e5542d2ce4b43d0b84c5c8fd461c
kimmypracha <pracha.promtaow@gmail.com>
2020-06-14 00:31:56 +0700

    add semicolon

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('./config/express');
 
 var db = mongoose();
 var app = express();
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 8080;
 app.listen(port,function(){
     console.log("App is running at port "+ port);
 });
