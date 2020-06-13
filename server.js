b482f257950133330f3714a77e1025741e936652
kimmypracha <pracha.promtaow@gmail.com>
2020-06-14 00:31:32 +0700

    change port to be more flexible

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('./config/express');
 
 var db = mongoose();
 var app = express();
-var port = 8000;
+var port = process.env.PORT || 8080
 app.listen(port,function(){
     console.log("App is running at port "+ port);
 });
