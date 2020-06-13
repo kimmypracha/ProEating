1e0ab737442e22d5bc30a708c1cb9da5e2aaa2f3
kimmypracha <pracha.promtaow@gmail.com>
2020-06-14 01:22:47 +0700

    get back to the same state

--- config/config.js
+++ config/config.js
@@ -1,2 +1,2 @@
-//module.exports = require('./env/'+ process.env.NODE_ENV + ".js");
-module.exports = require('./env/production.js');
+module.exports = require('./env/'+ process.env.NODE_ENV + ".js");
+//module.exports = require('./env/production.js');
