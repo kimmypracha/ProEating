d96e580f7bc8887b43dabbe068040f791c4b78db
kimmypracha <pracha.promtaow@gmail.com>
2020-06-14 01:28:46 +0700

    change mongodb_uri to variable

--- config/env/production.js
+++ config/env/production.js
@@ -1,4 +1,4 @@
 module.exports = {
     debug : false,
-    mongoUri : 'mongodb://heroku_gzvnl6hh:jba968usbh58i073d32ca8odqc@ds221435.mlab.com:21435/heroku_gzvnl6hh'
+    mongoUri : process.env.MONGODB_URI
 }
